c5bbcc52379af672afd455bc0dc751c9198f651b
Vlad <example@gmail.com>
2021-07-18 07:34:55 +0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+console.log('arr' + "- object");
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;
+
+//console.log(++incr);
+//console.log(--decr);
+
+//console.log(5%2);
+console.log(2 + 2 * 4 != '6');
+
+//const isChecked = true,
+//      isClosed = false;
+//console.log(isChecked || isClosed);
